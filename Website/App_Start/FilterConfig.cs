0cb8f56c5a88ebf47d4e6d1e9d46775c1414de9e
Woland2k <sasha@virtoway.com>
2014-12-01 16:28:33 -0800

    - initial checkin

---
+++ Website/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace VirtoCommerce
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Website/Controllers/PageController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MarketplaceWeb.Controllers
+{
+    using System.Web.Mvc;
+
+    [RoutePrefix("")]
+    public class PageController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("{pagename}")]
+        public ActionResult DisplayPage(string pageName)
+        {
+            return View(pageName);
+        }
+    }
+}
