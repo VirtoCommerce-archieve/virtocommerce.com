2f29345b17cc5dcab539fa0345625e408f1c5a70
EugeneOkhriemnko <evgokhrimenko@gmail.com>
2014-12-08 09:13:55 +0300

    send grid

--- Website/Controllers/MailController.cs
+++ Website/Controllers/MailController.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using SendGrid;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
+using System.Net;
 using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
@@ -16,31 +18,22 @@ namespace VirtoCommerce.Controllers
 		[ValidateAntiForgeryToken]
 		public ActionResult Send([ModelBinder(typeof(MailModelBinder))]MailModel model, bool isResend)
 		{
-			var smtp = new SmtpClient();
+			var username = ConfigurationManager.AppSettings["SendGridUsername"];
+			var password = ConfigurationManager.AppSettings["SendGridPassword"];
 
-
-			MailAddress from = new MailAddress(ConfigurationManager.AppSettings["FromEmail"]);
-			MailAddress to = new MailAddress(model.To);
-
-			MailMessage message = new MailMessage(from, to);
-
-			message.Subject = model.Subject;
-			message.Body = model.MailBody;
-
-			//smtp.Send(from, to, subject, mailbody);
-
+			SendGridMessage message = new SendGridMessage();
+			message.AddTo(model.To);
 			if(isResend)
 			{
-				from = new MailAddress(ConfigurationManager.AppSettings["FromEmail"]);
-				to = new MailAddress(ConfigurationManager.AppSettings["SupportToEmail"]);
-
-				message = new MailMessage(from, to);
-
-				message.Subject = model.Subject;
-				message.Body = model.MailBody;
-
-				//smtp.Send(from, to, subject, mailbody);
+				message.AddTo(ConfigurationManager.AppSettings["SupportToEmail"]);
 			}
+			message.From = new MailAddress(ConfigurationManager.AppSettings["FromEmail"]);
+			message.Subject = model.Subject;
+			message.Text = model.MailBody;
+
+			var credentials = new NetworkCredential(username, password);
+			var transportWeb = new Web(credentials);
+			transportWeb.Deliver(message);
 
 			return Json(new { }, JsonRequestBehavior.DenyGet);
 		}
