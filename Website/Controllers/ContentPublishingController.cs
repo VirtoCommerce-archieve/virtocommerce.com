806af428f39281bec65d601b47d3191f063d14b7
Woland2k <sasha@virtoway.com>
2014-12-10 14:08:19 -0800

    minor updates

---
+++ Website/Controllers/ContentPublishingController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace VirtoCommerce.Controllers
+{
+    public class ContentPublishingController : ApiController
+    {
+        // GET api/<controller>
+        public string Get(string name)
+        {
+            return "";
+            //return new string[] { "value1", "value2" };
+        }
+    }
+}
