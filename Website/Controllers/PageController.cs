41f99cdc062170d06251d10370e3fb015b7c0747
Woland2k <sasha@virtoway.com>
2014-12-13 02:19:35 -0800

    added support for page hierarchy

--- Website/Controllers/PageController.cs
+++ Website/Controllers/PageController.cs
@@ -26,8 +26,12 @@ namespace MarketplaceWeb.Controllers
 			var telemetry = new Microsoft.ApplicationInsights.TelemetryClient();
 			telemetry.TrackTrace("page request");
 
+		    if (String.IsNullOrEmpty(pageName))
+		    {
+		        pageName = "Index";
+		    }
 
-            // Check if physical page exists and use standard one, if not use content service
+		    // Check if physical page exists and use standard one, if not use content service
             var viewName = pageName.Replace("-", string.Empty);
             var result = ViewEngines.Engines.FindView(this.ControllerContext, viewName, null);
 		    if (result == null || result.View == null)
@@ -35,13 +39,10 @@ namespace MarketplaceWeb.Controllers
                 var filesPath = HostingEnvironment.MapPath("~/App_Data/Contents");
                 var service = new ContentPublishingService(filesPath, new[] { new LiquidTemplateEngine(filesPath) });
 
-                var item = service.GetContentItem(String.Format("/pages/{0}", pageName));
+                var item = service.GetContentItem(pageName.Contains("/") ? pageName : String.Format("/pages/{0}", pageName));
                 if(item != null)
 		         return this.View(item.Layout, item);
 		    }
-
-		    if (String.IsNullOrEmpty(pageName))
-		        viewName = "Index";
 
             return View(viewName);
 		}
