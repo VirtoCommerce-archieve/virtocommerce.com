97050d1d6ac2c93ba56c1a04fe2f63ea172e3c1b
Woland2k <sasha@virtoway.com>
2014-12-17 00:41:09 -0800

    - added unit tests

---
+++ Admin/Modules/Content/ContentModule.Tests/ContentScenarios.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.Results;
+using VirtoCommerce.ContentModule.Web.Controllers.Api;
+using VirtoCommerce.ContentModule.Web.Model;
+using Xunit;
+
+namespace ContentModule.Tests
+{
+    public class ContentScenarios
+    {
+        [Fact]
+        public void Can_get_collections()
+        {
+            var controller = new PublishingContentController();
+            var result = controller.GetCollections();
+            Assert.IsType<OkNegotiatedContentResult<CollectionItem[]>>(result);
+        }
+
+        [Fact]
+        public void Can_get_collection_items()
+        {
+            var controller = new PublishingContentController();
+            var result = controller.GetCollectionItems("Pages");
+            Assert.IsType<OkNegotiatedContentResult<ContentItem[]>>(result);
+        }
+    }
+}
