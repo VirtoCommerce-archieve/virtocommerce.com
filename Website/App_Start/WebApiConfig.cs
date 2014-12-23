4ac754ee8f48d7d20d67229e2873df35b9f977ed
EugeneOkhriemnko <evgokhrimenko@gmail.com>
2014-12-23 17:17:05 +0300

    merge

---
+++ Website/App_Start/WebApiConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VirtoCommerce.App_Start
+{
+    using System.Web.Http;
+
+    public class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Attribute routing.
+            config.MapHttpAttributeRoutes();
+        }
+
+    }
+}
---
+++ Website/Controllers/DeploymentController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace VirtoCommerce.Controllers
+{
+    using System.IO;
+
+    [RoutePrefix("api/deploy")]
+    public class DeploymentController : ApiController
+    {
+        [Route("")]
+        [HttpPost]
+        public async Task<HttpResponseMessage> Deploy(string token)
+        {
+            await DeployGit();
+
+            return new HttpResponseMessage(HttpStatusCode.OK);
+        }
+
+        public async Task DeployGit()
+        {
+            using (var client = new HttpClient())
+            {
+                var authToken = ConfigurationManager.AppSettings["DeployAuthenticationToken"];
+                var scmUrl = "https://virtocommerce-public.scm.azurewebsites.net/deploy";
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", authToken);
+                var deployResponse = await client.PostAsJsonAsync(scmUrl, new { format = "basic", url = "https://github.com/VirtoCommerce/virtocommerce.com.git" });//.GetAsync(scmUrl);//.PutAsJsonAsync(scmUrl, new StringContent("{}", Encoding.UTF8, "application/json"));
+                //var output = await deployResponse.Content.ReadAsStringAsync();
+            }
+        }
+    }
+}
